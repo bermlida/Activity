f248db22cb80d5543babe19f093e852680c7ae9c
Bermlida <fir4268@gmail.com>
2017-04-06 01:41:49 +0800

    adjust account info edit page style.
    1. adjust account info edit page style.
    2. adjust account info edit page feature.

--- app/Http/Controllers/Account/InfoController.php
+++ app/Http/Controllers/Account/InfoController.php
@@ -15,7 +15,7 @@ use App\Models\User;
 class InfoController extends Controller
 {
     /**
-     * 顯示帳戶資訊及基本資料編輯畫面。
+     * 顯示帳戶基本資料編輯畫面。
      *
      * @return \Illuminate\Http\Response
      */
@@ -29,47 +29,40 @@ class InfoController extends Controller
     }
 
     /**
-     * 回傳儲存帳戶及基本資料時，請求資料的驗證結果。
+     * 回傳儲存帳戶基本資料時，請求資料的驗證結果。
      *
      * @return bool
      */
     protected function validator(array $data)
     {
-        $account_rule = [
-            'password' => 'sometimes|min:6|confirmed',
-            'info_id' => 'required|numeric|exists:accounts,id',
-            'role_id' => 'required|numeric|exists:accounts'
-        ];
+        $account = Auth::user();
 
-        if (isset($data['role_id'])) {
-            switch ($data['role_id']) {
-                case 1:
-                    $profile_rule = [
-                        'name' => 'required|string|max:128',
-                        'mobile_phone' => 'sometimes|string|max:30'
-                    ];
-                    break;
-                case 2:
-                    $profile_rule = [
-                        'name' => 'required|string|max:255',
-                        'address' => 'required|string|max:512',
-                        'phone' => 'required|string|max:30',
-                        'fax' => 'sometimes|string|max:30',
-                        'mobile_phone' => 'sometimes|string|max:30',
-                        'intro' => 'required|string'
-                    ];
-                    break;
-                default:
-                    break;
-            }
+        switch ($account->role_id) {
+            case 1:
+                $rules = [
+                    'name' => 'required|string|max:128',
+                    'mobile_phone' => 'sometimes|string|max:30'
+                ];
+                break;
+            case 2:
+                $rules = [
+                    'name' => 'required|string|max:255',
+                    'address' => 'required|string|max:512',
+                    'phone' => 'required|string|max:30',
+                    'fax' => 'sometimes|string|max:30',
+                    'mobile_phone' => 'sometimes|string|max:30',
+                    'intro' => 'required|string'
+                ];
+                break;
+            default:
+                break;
         }
-
-        $profile_rule = $profile_rule ?? [];
-        return Validator::make($data, array_merge($account_rule, $profile_rule));
+        
+        return Validator::make($data, $rules);
     }
 
     /**
-     * 儲存帳戶資訊及基本資料
+     * 儲存帳戶基本資料
      *
      * @return \Illuminate\Http\Response
      */
@@ -83,40 +76,36 @@ class InfoController extends Controller
             );
         }
 
-        $info = Account::find($request->info_id);
-        if ($request->has('password') && !empty($request->password)) {
-            $info->password = bcrypt($request->password);
-        }
+        $account = Auth::user();
 
-        switch ($request->role_id) {
-            case 1:
-                $profile = $request->has('profile_id')
-                            ? User::find($request->profile_id)->fill($request->all())
-                            : new User($request->all());
-                break;
-            case 2:
-                $profile = $request->has('profile_id')
-                            ? Organizer::find($request->profile_id)->fill($request->all())
-                            : new Organizer($request->all());
-                break;
-            default: break;
-        }
-
-        $info_result = $info->save();
-        $profile_result = $profile->save();
-        if (is_null($info->profile)) {
-            $related_result = $profile->account()->save($info);
+        if (is_null($account->profile)) {
+            switch ($account->role_id) {
+                case 1:
+                    $profile = new User($request->all());
+                    break;
+                case 2:
+                    $profile = new Organizer($request->all());
+                    break;
+                default:
+                    break;
+            }
         } else {
-            $related_result = true;
+            $profile = $account->profile->fill($request->all());
         }
 
-        $data['info'] = $info;
-        $data['profile'] = $profile;
-        $data['save_result']['result'] = $info_result && $profile_result && $related_result;
-        $data['save_result']['message'] = $data['save_result']['result']
-            ? '儲存成功'
-            : '儲存失敗';
-        
-        return view('account.info', $data);
+        if ($profile->save()) {
+            if (!is_null($account->profile) || $profile->account()->save($account)) {
+                return redirect('/account/info')->with([
+                    'message_type' => 'success',
+                    'message_body' => '儲存成功'
+                ]);
+            }
+        }
+
+        $request->session()->flash('message_type', 'warning');
+
+        $request->session()->flash('message_body', '儲存失敗');
+
+        return view('account.info');
     }
 }
