eaa891b0e44627e79ff9ec23af90ae86780bee51
Bermlida <fir4268@gmail.com>
2019-06-18 00:23:26 +0800

    use env value to set service config value.

--- config/services.php
+++ config/services.php
@@ -36,25 +36,20 @@ return [
     ],
 
     'facebook' => [
-        'client_id' => '1300993716649012',
-        'client_secret' => '6f3e4abf05b2ce66923dea15d30b3bcf',
-        'redirect' => 'http://activity.apps/social-auth/facebook/reply',
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT'),
     ],
 
     'google' => [
-        'client_id' => '543556594150-5gb2jddp31iudbp7j76pd4l9gei3n8ag.apps.googleusercontent.com',
-        'client_secret' => 'CZ6M13XdGwAbZIqgcxOfgJgb',
-        'redirect' => 'http://activity.apps/social-auth/google/reply',
-    ],
-
-    'mailgun' => [
-        'domain' => 'sandbox9ebef659e197489989fb3166ad4f921c.mailgun.org',
-        'secret' => 'key-6d2d641fd8d31f248dc2ac809ae138e0',
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT'),
     ],
 
     'nexmo' => [
-        'key' => '5d5fb54c',
-        'secret' => 'a309e33dbc237da1',
+        'key' => env('NEXMO_KEY'),
+        'secret' => env('NEXMO_SECRET'),
         'sms_from' => '15556666666',
     ],
 ];
