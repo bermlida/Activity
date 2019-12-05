32a62f09d381d3db701318d93f84e798995ff1f1
Bermlida <fir4268@gmail.com>
2019-12-06 01:13:30 +0800

    set force protocol by parse app url in app config.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $protocol = Str::startsWith(config('app.url'), 'https://') ? 'https' : 'http';
+
+        URL::forceSchema($protocol);
     }
 
     /**
